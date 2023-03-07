c34e6c7602b2a6f15ad1e0e9ee74e010cb8f5839
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-03-07 16:58:09 +0100

    Create trimmer decorator class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,15 @@
+require_relative './base_decorator.rb'
+
+class TrimmerDecorator < Decorator
+    def initialize(nameable)
+        super(nameable)
+        @nameable = nameable
+    end
+
+    def correct_name
+        trimmed_name = @nameable.correct_name
+        trimmed_name = trimmed_name.slice(0, 10) if trimmed_name.length > 10
+        trimmed_name
+    end
+end
+
