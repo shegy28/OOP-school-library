9ecf77acd0b40e166d5cead0399044a39011dd1c
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-03-08 18:40:02 +0100

    fix association error

--- student.rb
+++ student.rb
@@ -3,15 +3,19 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, name, parent_permission: true)
+    super(name)
+    @age = age
+    @parent_permission = parent_permission
+    @name = name
   end
+
 
   def play_hooky
     '¯\(ツ)/¯'
   end
 
-  def add_classroom=(classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
