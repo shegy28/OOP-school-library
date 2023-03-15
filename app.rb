7c6d986ef223cf0ac7bb222bf161c82428bd0f38
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-03-15 08:43:40 +0100

    Add preserve data method to save data

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require './teacher'
 require './rental'
 require './classroom'
 require './nameable'
+require './library_management_system'
 
 class App
   def initialize
@@ -14,18 +15,26 @@ class App
   end
 
   def list_all_books
+    @book = load_data('books.json')
     if @book.empty?
       puts 'No books available'
     else
-      @book.each { |book| puts Title: book.title, Author: book.author }
+      data = @book.map(&:to_h)
+      data.each do |book|
+       puts "Title: #{book["title"]}, Author: #{book["author"]}"
+      end
     end
   end
 
   def list_all_people
+    @people = load_data('people.json')
     if @people.empty?
       puts 'No people available'
     else
-      @people.each { |person| puts Name: person.name, ID: person.id, Age: person.age }
+      data = @people.map(&:to_h)
+      data.each do |person|
+        puts "Name: #{person["name"]}, ID: #{person["id"]}, Age: #{person["age"]}"
+      end
     end
   end
 
@@ -51,6 +60,7 @@ class App
 
     student = Student.new(age, name, parents_permission)
     @people.push(student)
+    save_data(@people , 'people.json' )
     puts 'Student created successfully'
   end
 
@@ -64,6 +74,7 @@ class App
 
     teacher = Teacher.new(age, name, specialization)
     @people.push(teacher)
+    save_data(@people, 'people.json' )
     puts 'Teacher created successfully'
   end
 
@@ -74,36 +85,41 @@ class App
     author = gets.chomp
 
     book = Book.new(title, author)
-    @book.push(book)
+    @book << book
+    save_data(@book, "books.json")
     puts 'Book created successfully'
   end
 
   def create_rental
     puts 'Select a book from the following list by number'
+    @book = load_data("books.json")
     @book.each_with_index do |book, index|
-      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+      puts "#{index}) Title: #{book['title']}, Author: #{book['author']}"
     end
     rental_book = gets.chomp.to_i
-
+  
     puts 'Select a person from the following list by number'
+    @people = load_data("people.json")
     @people.each_with_index do |person, index|
-      puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      puts "#{index}) Name: #{person['name']}, ID: #{person['id']}, Age: #{person['age']}"
     end
     rental_person = gets.chomp.to_i
-
+  
     print 'Date: '
     date = gets.chomp
-
+  
     rental = Rental.new(date, @book[rental_book], @people[rental_person])
     @rental.push(rental)
+    save_data(@rental, 'rental.json')
     puts 'Rental created successfully'
   end
 
   def list_all_rentals
+    @rental = load_data('rental.json')
     print 'ID of person: '
     id = gets.chomp.to_i
     @rental.each do |rental|
-      puts "Date: #{rental.date}, Book: #{rental.book.title}, by #{rental.book.author}" if id == rental.person.id
+      puts "Date: #{rental['date']}, Book: #{rental['book']['title']}, by #{rental['book']['author']}" if id == rental['person']['id']
     end
   end
 end
